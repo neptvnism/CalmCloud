976f1a253dc00022591f008529df8916751a715a
neptune <113087621+neptvnism@users.noreply.github.com>
2024-07-07 12:10:49 +0300

    breathing exercises

---
+++ script.js
@@ -0,0 +1,65 @@
+let currentStep = 1;
+let currentExercise = '';
+const exercises = {
+    diaphragmatic: 5,
+    478: 4,
+    box: 5,
+    mindful: 5,
+    counting: 3,
+    progressive: 5,
+};
+
+function showSteps(exercise) {
+    currentExercise = exercise;
+    currentStep = 1;
+    document.querySelector('.exercise-buttons').style.display = 'none';
+    document.getElementById('steps-container').classList.remove('hidden');
+    showStep(currentStep);
+}
+
+function showStep(step) {
+    const stepElement = document.getElementById(`${currentExercise}-step-${step}`);
+    stepElement.classList.add('visible');
+    stepElement.classList.remove('hidden');
+    typeEffect(stepElement);
+
+    if (step < exercises[currentExercise]) {
+        document.getElementById('next-button').classList.remove('hidden');
+    } else {
+        document.getElementById('next-button').classList.add('hidden');
+    }
+}
+
+function nextStep() {
+    const currentStepElement = document.getElementById(`${currentExercise}-step-${currentStep}`);
+    currentStepElement.classList.remove('visible');
+    currentStepElement.classList.add('hidden');
+    currentStep++;
+    if (currentStep <= exercises[currentExercise]) {
+        showStep(currentStep);
+    }
+}
+
+function typeEffect(element) {
+    const text = element.textContent;
+    element.textContent = '';
+    element.classList.add('typing');
+    let i = 0;
+
+    function type() {
+        if (i < text.length) {
+            element.textContent += text.charAt(i);
+            i++;
+            setTimeout(type, 50); // Adjust typing speed here
+        } else {
+            element.classList.remove('typing');
+            element.style.whiteSpace = 'normal';
+        }
+    }
+
+    type();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('#steps-container').classList.add('hidden');
+});
